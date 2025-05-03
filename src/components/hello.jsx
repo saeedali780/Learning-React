2c0866b8f801d76bd07eb4153eeb20210c9ecb5a
Saeed Ali <saeedaliofficial77@gmail.com>
2025-05-04 03:45:49 +0500

    react started understanding the Jsx

---
+++ src/components/hello.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const myName= "SAEED";
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello Saeed</h1>
+      <h2>{myName}</h2>
+    </div>
+  )
+}
+
+export default Hello
