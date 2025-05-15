2f66801a6b93e6b9c4b50a57f6c22dc17ef339d5
Saeed Ali <saeedaliofficial77@gmail.com>
2025-05-16 02:57:31 +0500

    creating multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 //import ConditionalComponent from './components/ConditionalComponent'
-
-import Fruits from './components/Fruits.jsx'
-import Message from './components/Message.jsx'
+// import Fruits from './components/Fruits.jsx'
+// import Message from './components/Message.jsx'
+import Counter from './components/Counter.jsx'
 
 const App = () => {
 
   return (
     <div>
+      <Counter/>
       {/* <ConditionalComponent/> */}
       {/* <Message/>
       <Fruits /> */}
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Counter = () => {
+const [count,setCount] = useState(0);
+
+const [increamentBy,setIncrementBy] = useState(1)
+    const increament = ()=>{
+        setCount(count + increamentBy)
+    };
+
+    const decrement = ()=>{
+        setCount(count - increamentBy)
+    };
+
+    const increaseIncrement = ()=>{
+        setIncrementBy(increamentBy+1)
+    };
+    const decreaseIncrement = ()=>{
+        setIncrementBy(increamentBy - 1)
+    }
   return (
     <div>
-      
+        <h1>count value is:{count} </h1>
+        <button onClick={increament}>Increament</button>
+        <button onClick={decrement}>Decrement</button>
+
+        <h1>We are increamenting the value By:{increamentBy}</h1>
+        <button onClick={increaseIncrement}>increase Increment</button>   
+        <button onClick={decreaseIncrement}>decrease Decrement</button>   
+
     </div>
   )
 }
